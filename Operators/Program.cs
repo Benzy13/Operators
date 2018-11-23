77ae6f2f28a8acdddcd21f9bc55ce93c1d31704f
Benzy13 <jean@primelogic.co.za>
2018-11-23 10:07:05 +0200

    awe

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -10,6 +10,12 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            Console.WriteLine((float)a/(float)b);
+            Console.WriteLine(a + b * c);
+            Console.WriteLine(c<a && c>b);
         }
     }
 }
